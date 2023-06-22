6bf9ec31c36a662319a729b4a1f8e9483f93295d
Nicky <112414443+Kirkendolled@users.noreply.github.com>
2023-06-22 11:41:34 -0500

    asd

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+const express = require('express');
+const app = express();
+const port = 3000; // Replace with your desired port number
 
-    var formElement = event.target;
-    var formData = new FormData(formElement);
+// Configure Express to parse JSON data
+app.use(express.json());
 
-    // Send form data to JotForm
-    axios.post('https://form.jotform.com/231306033534040', formData)
-      .then(function(response) {
-        // Handle successful submission
-        var data = response.data; // Optional: Retrieve response data from JotForm
+// Handle the JotForm webhook request
+app.post('/webhook', (req, res) => {
+  const formData = req.body; // Access the form data sent by JotForm
 
-        // Populate the form answers into the document
-        document.getElementById('name').textContent = formData.get('name');
-        document.getElementById('email').textContent = formData.get('email');
-        document.getElementById('message').textContent = formData.get('message');
-      })
-      .catch(function(error) {
-        // Handle submission error
-        console.error('Form submission failed:', error);
-      });
-  });
+  // Process the form data and perform desired actions
+  // You can generate the document or store the data as needed
+
+  // Return a response to JotForm (status 200 OK)
+  res.sendStatus(200);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
